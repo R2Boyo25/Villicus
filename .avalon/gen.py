7bd01fdd7655f8e67f6b3e76aaf6d0fd1950a8a8
R2boyo25 <r2boyo25@gmail.com>
2021-12-19 17:56:40 -0600

    Remove the other quote

--- .avalon/gen.py
+++ .avalon/gen.py
@@ -4,4 +4,4 @@ binf = sys.argv[1]
 filesFolder = sys.argv[2]
 
 with open(binf, 'w') as f:
-    f.write(f'#!/bin/bash\ncd "{filesFolder}"\n"python3 "{filesFolder}/main.py" "$@"')
+    f.write(f'#!/bin/bash\ncd "{filesFolder}"\npython3 "{filesFolder}/main.py" "$@"')
